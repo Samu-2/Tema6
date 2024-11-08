660c85e6998c887621320c2495d0ea5381536535
Samu-2 <samuel.m27a@gmail.com>
2024-11-08 08:55:09 +0100

    Inconsistent merge state: could not find file MERGE_MSG in .git folder. This file contains the commit message for a merge commit. vale

--- Tema6(Strings)/src/ejercicios/Ej10.java
+++ Tema6(Strings)/src/ejercicios/Ej10.java
@@ -2,24 +2,24 @@ package ejercicios;
 
 import java.util.Scanner;
 
-
-
 public class Ej10 {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int opcion = 0;
-		String cadena= dimeCadenaTexto("Introduce texto", sc); 
+		int num1 = 0;
+		int num2 = 0;
+		String cadena = dimeCadenaTexto("Introduce texto", sc);
+
 		do {
-		
-			System.out.println("1.Subcadena/n2.Remplazo/n3.Impares/n0.Salir");
-	
+
+			System.out.println("\n1.Subcadena\n 2.Remplazo\n 3.Impares \n0.Salir");
+
 			opcion = dimeEntero("Elige una opción", sc);
 
 			switch (opcion) {
 			case 1:
-				System.out.println();
-				break;
+				System.out.println(subcadena(num1, num2, cadena, sc));
 
 			case 2:
 				System.out.println();
@@ -28,33 +28,32 @@ public class Ej10 {
 				System.out.println();
 				break;
 
-
 			case 0:
 				break;
-				default:
-					System.out.println("Opción no valida");
+			default:
+				System.out.println("Opción no valida");
 			}
 
 		} while (opcion != 0);
 
 	}
 
-	
-public static int dimeEntero(String texto, Scanner sc) { // para validar
-	do {
-		try {
+	public static int dimeEntero(String texto, Scanner sc) { // para validar
+		do {
+			try {
 
-			System.out.println(texto);
-			String orden = sc.nextLine();
-			int num = Integer.parseInt(orden);
-			return num;
-		} catch (Exception e) {
-			// TODO: handle exception
-			System.out.println("Introduce un número,no letras");
-		}
+				System.out.println(texto);
+				String orden = sc.nextLine();
+				int num = Integer.parseInt(orden);
+				return num;
+			} catch (Exception e) {
+				// TODO: handle exception
+				System.out.println("Introduce un número,no letras");
+			}
 
-	} while (true);
-}
+		} while (true);
+	}
+
 	public static String dimeCadenaTexto(String texto, Scanner sc) {
 		do {
 			try {
@@ -70,4 +69,23 @@ public static int dimeEntero(String texto, Scanner sc) { // para validar
 
 		} while (true);
 	}
-}
+
+	public static String subcadena(int num1, int num2, String cadena, Scanner sc) {
+		int len = cadena.length();
+		do {
+			num1 = dimeEntero("Introduce un número", sc);
+			num2 = dimeEntero("Introduce un número", sc);
+
+		} while (!((num1 > 0 && num1 <= len) && (num2 > 0 && num2 <= len && num2 > num1) && (num1 != num2)));
+
+		return cadena.substring(num1, num2);
+	}
+//crear funcion que devuelva las 2 primeras letras
+	//public static String reemplazo(String cadena) {
+		//int lenCadena = cadena.length();
+		//if (lenCadena > 10) {
+
+			
+		
+	}
+
--- Tema6(Strings)/src/ejercicios/sucio.java
+++ Tema6(Strings)/src/ejercicios/sucio.java
@@ -1,10 +1,32 @@
 package ejercicios;
 
+import java.time.LocalDateTime;
+import java.time.Month;
+
 public class sucio {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		LocalDateTime salidaMadrid = LocalDateTime.of(2024, Month.FEBRUARY, 4, 10, 30);
+		LocalDateTime llegadaNuevaYork = LocalDateTime.of(2024, Month.FEBRUARY, 4, 13, 30);
+System.out.println(salidaMadrid);
+System.out.println(llegadaNuevaYork);
 
-	}
 
+String cadena = "Hola buenos días";
+
+
+for (int i = 0; i < cadena.length(); i++) {
+	if(i==1) {
+	System.out.println(cadena.substring(i)); 
+}else
+	System.out.println("na");
 }
+	}
+}
+//System.out.println(s.substring(i,i+1));		//con substring tendríamos un String
+
+
+	
+
+
